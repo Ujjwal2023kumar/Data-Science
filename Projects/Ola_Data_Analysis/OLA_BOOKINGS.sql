37780332f62ab054bd7102bc9ae3d6b76fcb0adf
UJJWAL KUMAR <164533721+Ujjwal2023kumar@users.noreply.github.com>
2025-10-17 20:56:51 +0530

    Update OLA_BOOKINGS.sql

--- Projects/Ola_Data_Analysis/OLA_BOOKINGS.sql
+++ Projects/Ola_Data_Analysis/OLA_BOOKINGS.sql
@@ -1,7 +1,7 @@
 -- OLA Data Analyst Project - SQL Queries
-
 CREATE DATABASE Ola;
 USE Ola;
+
 
 -- Retrieve all successful bookings
 CREATE VIEW Successful_Bookings AS
@@ -9,8 +9,6 @@ SELECT *
 FROM OLA_BOOKINGS
 WHERE Booking_Status = 'Success';
 
--- Retrieve data
-SELECT * FROM Successful_Bookings;
 
 -- Find the average ride distance for each vehicle type
 CREATE VIEW Ride_Distance_For_Each_Vehicle AS
@@ -20,7 +18,6 @@ SELECT
 FROM OLA_BOOKINGS
 GROUP BY Vehicle_Type;
 
-SELECT * FROM Ride_Distance_For_Each_Vehicle;
 
 -- Get the total number of cancelled rides by customers
 CREATE VIEW Cancelled_Rides_By_Customers AS
@@ -29,7 +26,6 @@ SELECT
 FROM OLA_BOOKINGS
 WHERE Booking_Status = 'Cancelled by Customer';
 
-SELECT * FROM Cancelled_Rides_By_Customers;
 
 -- List the top 5 customers who booked the highest number of rides
 CREATE VIEW Top_5_Customers AS
@@ -41,7 +37,6 @@ GROUP BY Customer_ID
 ORDER BY Total_Rides DESC
 LIMIT 5;
 
-SELECT * FROM Top_5_Customers;
 
 -- Get the number of rides cancelled by drivers due to personal and car-related issues
 CREATE VIEW Rides_Cancelled_By_Drivers_P_C_Issues AS
@@ -50,7 +45,6 @@ SELECT
 FROM OLA_BOOKINGS
 WHERE Canceled_Rides_By_Driver = 'Personal & Car related issue';
 
-SELECT * FROM Rides_Cancelled_By_Drivers_P_C_Issues;
 
 -- Find the maximum and minimum driver ratings for Prime Sedan bookings
 CREATE VIEW Max_Min_Driver_Rating AS
@@ -60,15 +54,13 @@ SELECT
 FROM OLA_BOOKINGS
 WHERE Vehicle_Type = 'Prime Sedan';
 
-SELECT * FROM Max_Min_Driver_Rating;
 
 -- Retrieve all rides where payment was made using UPI
-6CREATE VIEW UPI_Payment AS
+CREATE VIEW UPI_Payment AS
 SELECT *
 FROM OLA_BOOKINGS
 WHERE Payment_Method = 'UPI';
 
-SELECT * FROM UPI_Payment;
 
 -- Find the average customer rating per vehicle type
 CREATE VIEW AVG_Cust_Rating AS
@@ -78,7 +70,6 @@ SELECT
 FROM OLA_BOOKINGS
 GROUP BY Vehicle_Type;
 
-SELECT * FROM AVG_Cust_Rating;
 
 -- Calculate the total booking value of rides completed successfully
 CREATE VIEW Total_Successful_Ride_Value AS
@@ -87,7 +78,6 @@ SELECT
 FROM OLA_BOOKINGS
 WHERE Booking_Status = 'Success';
 
-SELECT * FROM Total_Successful_Ride_Value;
 
 -- List all incomplete rides along with the reason
 CREATE VIEW Incomplete_Rides_Reason AS
@@ -97,4 +87,15 @@ SELECT
 FROM OLA_BOOKINGS
 WHERE Incomplete_Rides = 'Yes';
 
+
+
+SELECT * FROM Successful_Bookings;
+SELECT * FROM Ride_Distance_For_Each_Vehicle;
+SELECT * FROM Cancelled_Rides_By_Customers;
+SELECT * FROM Top_5_Customers;
+SELECT * FROM Rides_Cancelled_By_Drivers_P_C_Issues;
+SELECT * FROM Max_Min_Driver_Rating;
+SELECT * FROM UPI_Payment;
+SELECT * FROM AVG_Cust_Rating;
+SELECT * FROM Total_Successful_Ride_Value;
 SELECT * FROM Incomplete_Rides_Reason;
